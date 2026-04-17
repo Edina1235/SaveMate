d0b22d3bff375c298096649a28920df0b8cd46f1
Edina1235 <100123047+Edina1235@users.noreply.github.com>
2026-04-17 11:11:22 +0200

    Remove Firebase configuration from environment file
    Removed Firebase configuration details from environment.ts.

--- save-mate/environment.ts
+++ save-mate/environment.ts
@@ -1,11 +1,5 @@
 export const environment = {
     firebase: {
-        apiKey: "AIzaSyBbtXx0CV0C76m2x0hX4l8Vf3apnjplVXA",
-        authDomain: "savemate-af9d0.firebaseapp.com",
-        projectId: "savemate-af9d0",
-        storageBucket: "savemate-af9d0.firebasestorage.app",
-        messagingSenderId: "1003621324278",
-        appId: "1:1003621324278:web:f052ac4cd43a0e78ab23d7",
-        measurementId: "G-LP7DRWD00V"
+        ...
     }
 };
